be16c0baac5332a125a46ae960a9ef3c968e6dc5
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-05-04 08:29:14 -0500

    chore: Add initial project files and configurations

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public $userName, public $roomId,  public $message)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn() : Channel
+    {
+        return new Channel('chat.' . $this->roomId);
+    }
+    public function broadcastWith()
+    {
+        return [
+            'userName' => $this->userName,
+            'message' => $this->message,
+        ];
+    }
+}
